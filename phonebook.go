866754c1d3b3cb0fe0332f0a708c0b7989908108
Rafael Zaripov <rafaelzaripov@MacBook-Air-Rafael.local>
2024-04-24 00:12:45 +0500

    added random generate phonebook

--- phonebook.go
+++ phonebook.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strconv"
+	"math/rand"
+	"time"
 )
 
 type Entry struct {
@@ -13,6 +16,8 @@ type Entry struct {
 }
 
 var data = []Entry{}
+var MIN = 0
+var MAX = 26
 
 
 func search(key string) *Entry {
@@ -30,6 +35,36 @@ func list() {
 	}
 }
 
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func getString(len int64) string {
+
+	temp := ""
+	startChar := "!"
+	var i int64 = 1
+	for {
+		myRand := random(MIN, MAX)
+		newChar := string(startChar[0] + byte(myRand))
+		temp = temp + newChar
+		if i == len {
+			break
+		}
+		i ++
+	}
+	return temp
+}
+
+func populate(n int, s []Entry) {
+	for i := 0; i < n; i ++ {
+		name := getString(4)
+		surname := getString(5)
+		n := strconv.Itoa(random(100, 199))
+		data = append(data, Entry{name, surname, n})
+	}
+}
+
 func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
@@ -37,9 +72,12 @@ func main() {
 		fmt.Printf("Usage: %s search|list <arguments>\n", exe)
 		return
 	}
-	data = append(data, Entry{"Tom", "Hanks", "2239949224"})
-	data = append(data, Entry{"Stive", "Balmer", "343450453"})
-	data = append(data, Entry{"Johnny", "Deep", "24304204529"})
+	SEED := time.Now().Unix()
+	rand.Seed(SEED)
+
+	n := 100
+	populate(n, data)
+	fmt.Printf("Data has %d entries.\n", len(data))
 
 	switch arguments[1] {
 	case "search":
